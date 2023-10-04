96dbf3aa1d410e8b315f9bdbfef23ac1b5a03d1e
Yorim Knows <yohanfoo@gmail.com>
2023-10-03 21:46:01 -0400

    added whiteNoise Storages to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -155,6 +155,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
