162a22c55b2f650117cd8d01f2b4865e9af8241c
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 02:05:37 -0400

    added namespace to urls.py

--- runningApp/urls.py
+++ runningApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('logged-in/', views.logged_in_view, name='logged_in_view'),
-    path('accounts/', include('allauth.urls')),
+    #path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.socialaccount.urls', namespace='social')),
     # path('logout', LogoutView.as_view()), # make this view
 ]
