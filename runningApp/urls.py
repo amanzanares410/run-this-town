6f4a950885ae0d78f7d47a811b662f3b1aac4555
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 05:01:02 -0400

    adding logout button to logged_in.html

--- runningApp/urls.py
+++ runningApp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('logged-in/', views.logged_in_view, name='logged_in_view'),
     #path('accounts/', include('allauth.urls')),
-    path('logout', views.logout_view) # make this view
+    path('logout/', views.logout_view, name='logout') # make this view
 ]
