496965136dca5af0682111a2e94ea9af4a535f75
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 11:49:36 -0400

    added logic to make cs3240.super@gmail a default admin

--- runningApp/views.py
+++ runningApp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.models import Group
 
 
 
@@ -10,6 +11,12 @@ def index(request):
 
 #@login_required
 def logged_in_view(request):
+      # Check if the user's email is 'cs3240.super@gmail.com'
+    if request.user.email == 'cs3240.super@gmail.com':
+        # Fetch the Admins group
+        admin_group = Group.objects.get(name='Admins')
+        # Add the user to the Admins group
+        admin_group.user_set.add(request.user)
     if request.user.groups.filter(name='Admins').exists():
         # User is an admin
         return render(request=request, template_name="runningApp/logged_in_admin.html")
