c30ecff8a11219369b1178dfc67fba72f61056d5
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 02:58:12 -0400

    fixed syntax error in runningApp/urls.py

--- runningApp/urls.py
+++ runningApp/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path()
     path('logged-in/', views.logged_in_view, name='logged_in_view'),
     #path('accounts/', include('allauth.urls')),
     path('logout', views.logout_view) # make this view
