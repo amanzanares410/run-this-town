48affb436be72d8518421c0a0d6158193c96ad17
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 02:08:44 -0400

    changed the accounts/ in runningApps/urls.py

--- runningApp/urls.py
+++ runningApp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('logged-in/', views.logged_in_view, name='logged_in_view'),
     #path('accounts/', include('allauth.urls')),
-    path('accounts/', include('allauth.socialaccount.urls', namespace='social')),
+    path('accounts/', include(('allauth.socialaccount.urls', 'socialaccount'), namespace='social')),
     # path('logout', LogoutView.as_view()), # make this view
 ]
