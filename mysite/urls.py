555682a4c02a42d894066684287a5c1b9288fe89
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 04:43:51 -0400

    edited url config in root and funcs for views in runningApp

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from runningApp.views import logged_in_view 
 
 urlpatterns = [
+    path("", logged_in_view , name="logged_in_view"),
     path("runningApp/", include("runningApp.urls")),
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls'))
