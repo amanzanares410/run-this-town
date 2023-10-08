65d8743cc2c41abe5a83eb356a536d0545dfbd9d
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 05:49:50 -0400

    modified logged_in_view htmls to display different page for admins

--- runningApp/views.py
+++ runningApp/views.py
@@ -12,10 +12,10 @@ def index(request):
 def logged_in_view(request):
     if request.user.groups.filter(name='Admins').exists():
         # User is an admin
-        return render(request=request, template_name="admin_dashboard.html")
+        return render(request=request, template_name="runningApp/logged_in_admin.html")
     else:
         # User is a regular user
-        return render(request=request, template_name="runningApp/logged_in_admin.html")
+        return render(request=request, template_name="runningApp/logged_in.html")
 
 def logout_view(request):
     logout(request)
