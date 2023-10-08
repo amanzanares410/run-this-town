7949d959aa0852088a4b79dfae733c8f57cff1a5
Yorim Knows <yohanfoo@gmail.com>
2023-10-08 05:46:47 -0400

    fixed spelling error for group name  logged_in_view

--- runningApp/views.py
+++ runningApp/views.py
@@ -10,7 +10,12 @@ def index(request):
 
 #@login_required
 def logged_in_view(request):
-    return render(request=request, template_name="runningApp/logged_in.html")
+    if request.user.groups.filter(name='Admins').exists():
+        # User is an admin
+        return render(request=request, template_name="admin_dashboard.html")
+    else:
+        # User is a regular user
+        return render(request=request, template_name="runningApp/logged_in_admin.html")
 
 def logout_view(request):
     logout(request)
