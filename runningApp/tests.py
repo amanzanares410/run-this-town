01e3395c6c9373d0629a036a6d7f3961757f5e04
Akash <64038661+akashpamal@users.noreply.github.com>
2023-10-19 21:19:53 -0400

    added one always true test

--- runningApp/tests.py
+++ runningApp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        self.assertIs(1, 1)
+    
+class RouteTests(TestCase):
+    pass
