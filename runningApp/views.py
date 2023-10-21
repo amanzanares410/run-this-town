d193f90a4378a82db0543ed1e1d6f4246207ae8c
Ashley Manzanares <fnv2vx@virginia.edu>
2023-10-21 15:09:35 -0400

    changing views.py and adding template

--- runningApp/views.py
+++ runningApp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth.models import Group
-
+from django.conf import settings
+import requests
 
 
 def index(request):
@@ -29,4 +30,23 @@ def logout_view(request):
     return redirect('index')
 
 def weather_view(request):
-    return render(request, template_name="runningApp/weather.html")
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': 'Charlottesville, VA',
+        'appid': settings.WEATHER_API_KEY,
+        'units': 'metric'
+    }
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        context = {
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description']
+        }
+    else:
+        context = {
+            'error': 'Unable to fetch weather data for Charlottesville, VA.'
+        }
+
+    return render(request, 'runningApp/weather.html', context)
