498461f637074efdb0e839a011bbe6d9170724a9
Brian Baugher <bdb7hq@virginia.edu>
2023-10-24 18:38:11 -0400

    set up new models for route

--- runningApp/models.py
+++ runningApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Stop(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+class Route(models.Model):
+    user_id = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+    route_name = models.CharField(max_length=100)
+    route_description = models.CharField(max_length=500)
+    approved = models.BooleanField(default=False)
+    stops = models.ManyToManyField(Stop, through='RouteStop')
+
+class RouteStop(models.Model):
+    route_id = models.ForeignKey(Route, on_delete=models.CASCADE)
+    stop_id = models.ForeignKey(Stop, on_delete=models.CASCADE)
+    stop_order = models.IntegerField(default=0)
